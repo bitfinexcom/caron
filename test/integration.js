b9402608e7b7ab2d27b9fee17a5e9fc22d41cda5
Robert Kowalski <rok@kowalski.gd>
2017-12-13 18:19:33 +0100

    test: add first test

---
+++ test/integration.js
@@ -0,0 +1,52 @@
+/* eslint-env mocha */
+
+'use strict'
+
+const assert = require('assert')
+
+const Caron = require('../')
+const Redis = require('ioredis')
+const redis = new Redis()
+
+const Queue = require('bull')
+const testQueue = new Queue('default')
+
+after(() => {
+  redis.disconnect()
+})
+
+describe('integration', () => {
+  it('pulls stuff from redis', (done) => {
+    const caron = new Caron({
+      type: 'bull',
+      list: 'bull_test',
+      redis: 'redis://127.0.0.1:6379',
+      freq: 25,
+      batch: 100,
+      q_prefix: '',
+      q_lifo: false,
+      def_queue: 'default',
+      def_worker: 'BaseJob',
+      def_attempts: 1,
+      kill: false,
+      debug: false
+    })
+
+    testQueue.process((job, cb) => {
+      assert.equal(job.data.foo, 'bar')
+
+      cb(null)
+      testQueue.close().then(() => {
+        caron.stop(() => {
+          caron.redis.disconnect()
+          done()
+        })
+      })
+    })
+
+    caron.start()
+
+    const payload = JSON.stringify({ foo: 'bar', queue: 'default' })
+    redis.lpush('bull_test', payload)
+  })
+})
