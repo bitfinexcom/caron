170309be02e17836d7384fa8ac2eeb34dfbe75b1
Tarcisio Ferraz <tarcisio.ferraz@bitfinex.com>
2024-03-20 11:37:46 -0300

    check if delay is working

--- test/integration.js
+++ test/integration.js
@@ -196,6 +196,38 @@ describe('bull params', () => {
     redis.lpush('bull_test', payload)
   })
 
+  it('uses fixed backoff for failing task', (done) => {
+    const testQueue = new Queue('default')
+    const type = 'fixed'
+    const delay = Math.floor(Math.random() * 1000)
+    const attempts = 5
+    let currentAttempt = 0
+
+    testQueue.process((job, cb) => {
+      assert((Date.now() - start) >= (delay * currentAttempt))
+
+      cb(new Error())
+
+      currentAttempt++
+      if (currentAttempt === attempts) {
+        setImmediate(() => {
+          testQueue.close().then(() => {
+            caron.stop(() => {
+              caron.redis.disconnect()
+              done()
+            })
+          })
+        })
+      }
+    })
+
+    caron.start()
+
+    const payload = JSON.stringify({ foo: 'bar', $attempts: attempts, $backoff: { type, delay } })
+    const start = Date.now()
+    redis.lpush('bull_test', payload)
+  })
+
   it('uses exponential backoff method with given delay', (done) => {
     const testQueue = new Queue('default')
     const type = 'exponential'
